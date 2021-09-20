f3981b4d404fc0f81749c7dcb5854ba66e957b72
Williamsliam23 <williams.liam23@gmail.com>
2021-09-20 18:44:13 -0500

    tic-tac-toe AI start

--- W4/D1/skeleton/lib/tic_tac_toe_node.rb
+++ W4/D1/skeleton/lib/tic_tac_toe_node.rb
@@ -1,7 +1,11 @@
 require_relative 'tic_tac_toe'
+require_relative '00_tree_node'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @root_node = PolyTreeNode.new(board)
+    @next_mover_mark = next_mover_mark
+    @prev_mov_pos = prev_mov_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +17,6 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+
   end
 end
