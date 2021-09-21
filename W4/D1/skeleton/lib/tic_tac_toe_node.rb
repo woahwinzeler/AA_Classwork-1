307c467492557e46e08a4a84c9aa0923355157d3
RobertT122 <robert@tetrault.org>
2021-09-20 20:02:10 -0400

    children method

--- W4/D1/skeleton/lib/tic_tac_toe_node.rb
+++ W4/D1/skeleton/lib/tic_tac_toe_node.rb
@@ -1,11 +1,13 @@
 require_relative 'tic_tac_toe'
-require_relative '00_tree_node'
 
 class TicTacToeNode
+
+  attr_accessor :board, :next_mover_mark, :prev_move_pos
+  
   def initialize(board, next_mover_mark, prev_move_pos = nil)
-    @root_node = PolyTreeNode.new(board)
+    @board = board
     @next_mover_mark = next_mover_mark
-    @prev_mov_pos = prev_mov_pos
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -17,6 +19,14 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
-
+    possible_moves = []
+    board.rows.each_with_index do |square, i|
+      if square.empty?
+        possible_board = board.dup
+        possible_board[i] = next_mover_mark
+        possible_moves << possible_board
+      end
+    end
+    possible_moves
   end
 end
