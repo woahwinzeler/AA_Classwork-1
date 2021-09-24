15842dfebf359ef992e37af0d58e23494754245d
Williamsliam23 <williams.liam23@gmail.com>
2021-09-24 18:01:55 -0500

    anagram_2

--- W4/D5/big_o/anagrams.rb
+++ W4/D5/big_o/anagrams.rb
@@ -7,5 +7,14 @@ def first_anagram?(string_1, string_2)
   sub_array.flatten.include?(string_2)
 end
 
+def anagram_2(string_1, string_2)
+  letter1 = string_1.split('')
+  letter2 = string_2.split('')
+
+  letter1.each do |i|
+    i2 = letter2.find_index(i)
+  end
+end
+
 p first_anagram?("gizmo", "sally")    #=> false
 p first_anagram?("elvis", "lives")    #=> true
