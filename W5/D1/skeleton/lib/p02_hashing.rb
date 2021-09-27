e316b5b03e5ecdb1e519e9c7898ba937447f1ffd
Williamsliam23 <williams.liam23@gmail.com>
2021-09-27 17:13:46 -0500

    hashing Integer and String function add and complete

--- W5/D1/skeleton/lib/p02_hashing.rb
+++ W5/D1/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,28 @@ end
 
 class Array
   def hash
+    key = 0
+    copy = self.dup
+    until copy.empty?
+      key = key * 10
+      key += copy.shift
+    end
+    return key
   end
 end
 
 class String
   def hash
+    return -1 if self.nil?
+    alphabet = ('a'..'z').to_a
+    key = 0
+    copy = self.dup.downcase
+    until copy.empty?
+      key = key * 10
+      key += alphabet.index(copy[0]) + 1
+      copy = copy[1..-1]
+    end
+    return key
   end
 end
 
