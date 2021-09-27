cc6187895748beca2f56b34b7869361cd9433a03
Alan Chiu <ms0372631@gmail.com>
2021-09-27 14:11:44 -0700

    finished IntSet

--- W5/D1/skeleton/lib/p01_int_set.rb
+++ W5/D1/skeleton/lib/p01_int_set.rb
@@ -4,27 +4,23 @@ class MaxIntSet
 
   def initialize(max)
     @max = max
-    @store = Array.new(@max) { Array.new }
+    @store = Array.new(@max, false) 
   end
 
   def insert(num)
-    if num > max
+    if num > max || num < 0
       raise 'Out of bounds'
     else
-      if num > store.last
-        (num - store.last - 1).times { store << false }
-      end
       store[num] = true
-      return true
     end
   end
 
   def remove(num)
-    store.delete_at(num) if store.include?(num)
+    store[num] = false if store[num]
   end
 
   def include?(num)
-    store.include?(num)
+    store[num]
   end
 
   private
@@ -38,23 +34,30 @@ end
 
 
 class IntSet
+
+attr_reader :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    self[num].push(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    store[num % num_buckets]
   end
 
   def num_buckets
