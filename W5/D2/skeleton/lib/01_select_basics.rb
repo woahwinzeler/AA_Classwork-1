fbf8e154774bf35fca66f6fea33cc480b37950e5
shuang zheng <shuangzheng16@gmail.com>
2021-09-28 14:56:52 -0700

    gdp

--- W5/D2/skeleton/lib/01_select_basics.rb
+++ W5/D2/skeleton/lib/01_select_basics.rb
@@ -36,6 +36,9 @@ def per_capita_gdp
   # Show the name and per capita gdp (gdp/population) for each country where
   # the area is over 5,000,000 km^2
   execute(<<-SQL)
+  SELECT name, gdp/population
+  FROM countries
+  WHERE  area > 5,000,000
   SQL
 end
 
