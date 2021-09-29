0c7b96326799faa62f5e8c201c82d887700d34e2
Jshin720 <jeffshin720@gmail.com>
2021-09-29 17:09:00 -0400

    Working classes

--- W5/D3/aa_questions/questions_database.rb
+++ W5/D3/aa_questions/questions_database.rb
@@ -1,15 +1,34 @@
 require 'singleton'
+require 'sqlite3'
 
 class QuestionsDatabase < SQLite3::Database
-  include singleton
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
 
 
 end
 
 class Users
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    data.map { |options| Users.new(options) }
+  end
+
+  def initialize(options)
+    @fname = options['fname']
+    @lname = options['lname']
+    @id = options['id']
+  end
 
   def self.find_by_id()
 
   end
 
 end
+
+p Users.all
