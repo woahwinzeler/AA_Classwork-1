ac84414e1b0078c22f46ebe8fb01ea5a5a14dc11
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-10-06 14:47:50 -0700

    generated users routes and controller

--- W6/D3/routes_and_cont/routes_and_controllers/config/routes.rb
+++ W6/D3/routes_and_cont/routes_and_controllers/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users 
+
+  #equivlant to resources :users
+  get "/users", to: "users#index"
+  post "/users", to: "users#create"
+  get "/users/new", to: "users#new", as: "new_user"
+  get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  get "/users/:id", to: "users#show", as: "user"
+  patch "/users/:id", to: "users#edit"
+  put "/users/:id", to: "users#edit"
+  delete "/users/:id", to: "users#destroy"
+
+
+
 end
