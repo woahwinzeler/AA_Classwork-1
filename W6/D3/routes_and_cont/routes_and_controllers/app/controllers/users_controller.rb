f3a30e0a5d1fc5ae95687350e70924a0d1fb5ce4
Williamsliam23 <williams.liam23@gmail.com>
2021-10-06 17:06:10 -0500

    params requests, swap render routes

--- W6/D3/routes_and_cont/routes_and_controllers/app/controllers/users_controller.rb
+++ W6/D3/routes_and_cont/routes_and_controllers/app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
+    render json: params
   end
 
   def create
+    render json: params
   end
 
   def new
@@ -12,6 +15,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    render json: params
   end
 
   def destroy
